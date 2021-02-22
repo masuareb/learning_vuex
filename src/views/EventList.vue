f3757f9d4edb40c6212d06a59908caa68abf0e09
Arturo Suárez <masuareb@gmail.com>
2021-02-22 15:41:12 -0600

    Initial commit (Final version of previous course: "Real World Vue 2")

---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <p>
+    </p>
+  </div>
+
+</template>
+
+<script>
+import EventCard from "@/components/EventCard";
+import EventService from "@/services/EventService";
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.og('There was an error:', error.response)
+      })
+  }
+}
+</script>
