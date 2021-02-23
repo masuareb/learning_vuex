076145cca4aed12b0ef7bfc074843948c15cacf5
Arturo Suárez <masuareb@gmail.com>
2021-02-22 22:45:36 -0600

    Avoid duplicate API calls by means of a local search of the event by id

--- src/store/index.js
+++ src/store/index.js
@@ -51,13 +51,19 @@ export default new Vuex.Store({
           })
     },
     fetchEvent({ commit }, id) {
-      EventService.getEvent(id)
-          .then(response => {
-            commit('SET_EVENT', response.data)
-          })
-          .catch(error => {
-            console.log('There was an error:', error.response)
-          })
+      var event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      }
+      else {
+        EventService.getEvent(id)
+            .then(response => {
+              commit('SET_EVENT', response.data)
+            })
+            .catch(error => {
+              console.log('There was an error:', error.response)
+            })
+      }
     }
   },
   modules: {}
