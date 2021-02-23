de65e938545262b061d410d1072d306a6dfa38ae
Arturo Suárez <masuareb@gmail.com>
2021-02-22 22:21:15 -0600

    Added pagination and hyperlinks in list component to get next and previous pages

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,8 +2,10 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
-    <p>
-    </p>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 }}" rel="prev">Prev</router-link> |
+    </template>
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 }}">Next Page</router-link>
   </div>
 
 </template>
@@ -15,9 +17,17 @@ export default {
   components: {
     EventCard
   },
-  computed: mapState(['events']),
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events'])
+  },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
   }
 }
 </script>
