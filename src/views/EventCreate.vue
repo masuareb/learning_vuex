64740ca5eb40fafbd8a9527d5f58a36b23d93f55
Arturo Suárez <masuareb@gmail.com>
2021-02-22 18:20:51 -0600

    Define compute property to access store data

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Create Event, {{ $store.state.user.name }}</h1>
+  <div>
+    <h1>Create Event, {{ userName }}</h1>
+    <p>This event is created by {{ userName }}</p>
+  </div>
 </template>
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    }
+  }
+}
+</script>
